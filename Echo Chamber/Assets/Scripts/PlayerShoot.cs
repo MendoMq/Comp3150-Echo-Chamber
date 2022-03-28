795b8264b1346f2ef2bd4fb43b642172d81170e2
Zander <45277708+LonesomeDevil@users.noreply.github.com>
2022-03-28 20:58:34 +1100

    Shooting feature
    added raycast shooting.
    need to add effect to show shot
    tried adding muzzle flash found out doesnt work with shader, causes it to be purple

---
+++ Echo Chamber/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public float dmg = 10f;
+    public float range = 100f;
+    public float fireRate = 5f;
+
+    public Camera fpsCam;
+
+
+    private float nextTimeToFire = 0f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f/fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(dmg);
+            }
+        }
+    }
+}
