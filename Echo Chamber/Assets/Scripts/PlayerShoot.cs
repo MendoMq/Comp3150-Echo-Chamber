ef1ab7fa303b7826ef5091c86e2414fb5781ab0d
Zander <45277708+LonesomeDevil@users.noreply.github.com>
2022-03-29 18:12:33 +1100

    Enemy AI shooting
    Added it so that the enemy AI now shoots.

--- Echo Chamber/Assets/Scripts/PlayerShoot.cs
+++ Echo Chamber/Assets/Scripts/PlayerShoot.cs
@@ -25,7 +25,6 @@ public class PlayerShoot : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
-            Debug.Log(hit.transform.name);
 
             Target target = hit.transform.GetComponent<Target>();
             if (target != null)
