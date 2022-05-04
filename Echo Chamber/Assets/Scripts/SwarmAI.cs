7ad241ade8964b729af4a1f66c3967b545ce1113
MendoMq <69186120+MendoMq@users.noreply.github.com>
2022-05-04 22:48:53 +1000

    Added SwarmAI improvements + animation

--- Echo Chamber/Assets/Scripts/SwarmAI.cs
+++ Echo Chamber/Assets/Scripts/SwarmAI.cs
@@ -7,7 +7,14 @@ public class SwarmAI : MonoBehaviour
     public Transform target;
     public float speed;
     public float triggerLength = 4;
+    public float minDistance = 2;
     public float dmg;
+    public bool aiAgro = false;
+
+    int swapDir = 1;
+    float timer = 0;
+    float swapTime = 1f;
+
 
     void Start()
     {
@@ -17,11 +24,36 @@ public class SwarmAI : MonoBehaviour
     void Update()
     {
 
-        if (Vector3.Distance(target.position, transform.position) < triggerLength)
+        if (Vector3.Distance(target.position, transform.position) < triggerLength && aiAgro == false)
         {
-            transform.LookAt(target);
-            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            aiAgro = true;
+            SetSwapTime();
         }
+
+        if(aiAgro){
+            if(Vector3.Distance(target.position, transform.position) >= minDistance){
+                transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+            }else{
+                transform.position += new Vector3 (-transform.forward.x * speed * Time.deltaTime,0,-transform.forward.z * speed * Time.deltaTime);
+                transform.localPosition += transform.right * swapDir * speed * 1.5f * Time.deltaTime;
+            }
+            transform.LookAt(target);
+
+            if(timer >= swapTime){
+                swapDir = -swapDir;
+                SetSwapTime();
+                timer = 0;
+            }
+
+            timer += Time.deltaTime;
+
+        }
+
+
+    }
+
+    void SetSwapTime(){
+        swapTime = Random.Range(0.5f, 3f);
     }
 
     void OnDrawGizmosSelected()
