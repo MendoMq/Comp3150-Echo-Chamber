8f46cca91895bcbad43c1e19dbcdf657b52c4676
MendoMq <daniel.mendham@students.mq.edu.au>
2022-05-14 16:27:06 +1000

    Basic Enemy Changes and Better Target Script explosion

--- Echo Chamber/Assets/Scripts/EnemyAI.cs
+++ Echo Chamber/Assets/Scripts/EnemyAI.cs
@@ -42,16 +42,13 @@ public class EnemyAI : MonoBehaviour
     {
         transform.LookAt(target);
 
-        if (Time.time > nextFire)
+        if (Time.time >= nextFire)
         {
             nextFire = Time.time + fireRate;
-            GameObject Bullet = Instantiate(bullet);
+            GameObject Bullet = Instantiate(bullet, transform.position, Quaternion.identity);
             Physics.IgnoreCollision(Bullet.GetComponent<Collider>(), gunPos.parent.GetComponent<Collider>());
-            Bullet.transform.position = gunPos.position;
-            Vector3 rotation = Bullet.transform.rotation.eulerAngles;
-            Bullet.transform.rotation = Quaternion.Euler(rotation.x, transform.eulerAngles.y, rotation.z);
-            Bullet.GetComponent<Rigidbody>().AddForce(gunPos.forward * bulletSpeed, ForceMode.Impulse);
-            Bullet.GetComponent<Bullet>().dmg = dmg;
+            Bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed, ForceMode.Impulse);
+            //Bullet.GetComponent<Bullet>().dmg = dmg;
         }
         
     }
