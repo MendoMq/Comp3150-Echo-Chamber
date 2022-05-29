46c53ee4309b890014773f55e732085c6a7b7433
MendoMq <69186120+MendoMq@users.noreply.github.com>
2022-05-29 12:41:04 +1000

    Demo level tweaks and gradual Health Bar

--- Echo Chamber/Assets/Scripts/HealthBar.cs
+++ Echo Chamber/Assets/Scripts/HealthBar.cs
@@ -11,18 +11,33 @@ public class HealthBar : MonoBehaviour
     float currentHealth;
     float visualHealth;
 
+    void Start(){
+        slider.maxValue = 50;
+        slider.value = 50;
+        currentHealth = 50;
+        visualHealth = currentHealth;
+    }
+
     public void SetMaxHealth(float health)
     {
         slider.maxValue = health;
         slider.value = health;
+        currentHealth = health;
+        visualHealth = currentHealth;
     }
 
     public void SetHealth(float health)
     {
-        slider.value = health;
+        currentHealth = health;
     }
 
     void Update(){
+        if(currentHealth < visualHealth){
+            visualHealth -= sliderChange * Time.deltaTime;
+        }else if(currentHealth > visualHealth){
+            visualHealth += sliderChange * Time.deltaTime;
+        }
 
+        slider.value = visualHealth;
     }
 }
