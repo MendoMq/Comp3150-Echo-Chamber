705d1ab6a9e7854ed9e620fd805dfc8641239096
MendoMq <69186120+MendoMq@users.noreply.github.com>
2022-05-29 12:29:08 +1000

    Squashed commit of the following:
    commit b57d7fb89980d49ecbb5fbc498a96bcd1eebb7bf
    Author: MendoMq <69186120+MendoMq@users.noreply.github.com>
    Date:   Sun May 29 12:25:59 2022 +1000
    
        Health Bar tweaks
    
    commit 327d69385fc29d7a7d9d62700adce3a5f013e631
    Author: shankie-afk <69331772+shankie-afk@users.noreply.github.com>
    Date:   Sat May 28 18:06:31 2022 +1000
    
        HealthBar
    
    commit 45e54f502c609a5f2be8509920a56594e97c2fb4
    Author: shankie-afk <69331772+shankie-afk@users.noreply.github.com>
    Date:   Sat May 28 18:04:09 2022 +1000
    
        HealthBar

---
+++ Echo Chamber/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public float sliderChange;
+    float currentHealth;
+    float visualHealth;
+
+    public void SetMaxHealth(float health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void SetHealth(float health)
+    {
+        slider.value = health;
+    }
+
+    void Update(){
+
+    }
+}
